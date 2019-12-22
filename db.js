ca0c4ea287dffde16c40755744bfd5f4baa6e09a
theCaseFor <59127634+theCaseFor@users.noreply.github.com>
2019-12-22 03:30:03 -0600

    Create db.js

---
+++ db.js
@@ -0,0 +1,16 @@
+
+function saveToFirebase(email) {
+    var emailObject = {
+        email: email
+    };
+
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+        .then(function(snapshot) {
+            success(); // some success method
+        }, function(error) {
+            console.log('error' + error);
+            error(); // some error method
+        });
+}
+
+saveToFirebase(email);
